bbe0a770920290b429fa2381fb0f0e3f82280c3a
chillinhen <c.hillen@gmx.net>
2016-04-19 10:28:10 +0200

    responsive Todos

--- partials/nav-off.php
+++ partials/nav-off.php
@@ -1,10 +1,11 @@
 <nav id="offNav" class="responsiveMenu">
     <?php wp_nav_menu(array('theme_location' => 'meta-links', 'menu_class' => 'meta-menu', 'container' => '', 'fallback_cb' => '')); ?>
     <hr>
-    <a href="<?php echo home_url(); ?>">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
-                            </a>
-     <?php html5blank_nav(); ?>
-    
-        
+    <a class="off-logo-cnt" href="<?php echo home_url(); ?>">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
+    </a>
+    <?php html5blank_nav(); ?>
+    <?php wp_nav_menu(array('theme_location' => 'footer-links', 'menu_class' => 'footer-menu', 'container' => '', 'fallback_cb' => '')); ?>
+
+
 </nav>
